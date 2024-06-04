34df18b0c841212226ea4a0816addd0daea75e95
WelderOliveira <weldersilva326@gmail.com>
2024-06-03 23:47:25 -0300

    Adjust Docker Commands install dep

--- docker/scripts/entry.sh
+++ docker/scripts/entry.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+chown -R www-data:www-data /var/www/vendor
+chmod -R 755 /var/www/vendor
 
-npm install && npm run build
-composer install --ignore-platform-req
+composer clear-cache
+composer install --ignore-platform-req=ext-zip --prefer-dist --no-interaction --optimize-autoloader
 php artisan key:generate
 php artisan migrate:fresh --seed
 
 chmod -R 777 /var/www/storage && chmod -R 777 /var/www/bootstrap/cache
-php-fpm
